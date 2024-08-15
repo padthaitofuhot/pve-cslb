1a6e3ad8d7a3c18718f957ab15bfd820e6c6d174
padthaitofuhot <padthaitofuhot@users.noreply.github.com>
2024-08-14 22:42:17 -0400

    move configuration parsing into Runner.py

--- SimpleCSLB/Config.py
+++ SimpleCSLB/Config.py
@@ -12,16 +12,9 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-import re
-from os import environ
-from re import compile
-
 from loguru import logger
-from yaml import safe_load
 
 logger.disable("Config")
-
-pluralize_re_match = compile(r"^(include|exclude)_(node|type|vmid)$")
 
 
 class ConfigurationError(Exception):
@@ -29,7 +22,6 @@ class ConfigurationError(Exception):
 
 
 class Config:
-
     args: dict
     config_file: str
     dry_run: bool
@@ -39,16 +31,16 @@ class Config:
     exclude_nodes: list
     exclude_vmids: list
     exclude_types: list
-    percent_cpu: float
-    percent_mem: float
+    _percent_cpu: float
+    _percent_mem: float
     max_migrations: int
     proxmox_user: str
     proxmox_pass: str
     proxmox_node: str
     proxmox_port: int
 
-    def __init__(self, args: dict):
-
+    def __init__(self):
+        self.config_file = ""
         self.dry_run = False
         self.verbose = False
         self.quiet = False
@@ -56,111 +48,34 @@ class Config:
         self.exclude_nodes = []
         self.exclude_vmids = []
         self.exclude_types = []
-        self.percent_cpu = 0.4
-        self.percent_mem = 0.6
+        self._percent_cpu = 0.4
+        self._percent_mem = 0.6
         self.max_migrations = 5
         self.proxmox_user = "root@pam"
         self.proxmox_pass = ""
         self.proxmox_node = "localhost"
         self.proxmox_port = 8006
 
-        if args:
-            self.args = args
-            # First read from config file
-            if "config_file" in args.keys() and args["config_file"]:
-                logger.debug(f"CLI: Configured config_file = {args['config_file']}")
-                try:
-                    logger.debug(
-                        f"Reading YAML configuration from {args['config_file']}"
-                    )
-                    with open(args["config_file"], "r") as stream:
-                        config = safe_load(stream)
-                        for k, v in config.items():
-                            setattr(self, k, v)
-                            logger.debug(
-                                f"YML: Configured {k} = {'*****' if k == 'proxmox_pass' else v}"
-                            )
-                except FileNotFoundError:
-                    raise ConfigurationError("Config file not found")
+    @property
+    def percent_cpu(self) -> float:
+        return self._percent_cpu
 
-            # Second override from environment variables
-            for var in [
-                "PROXMOX_NODE",
-                "PROXMOX_PORT",
-                "PROXMOX_USER",
-                "PROXMOX_PASS",
-                "PERCENT_CPU",
-                "PERCENT_MEM",
-                "MAX_MIGRATIONS",
-                "VERBOSE",
-                "QUIET",
-                "DRY_RUN",
-                "NO_COLOR",
-            ]:
-                if environ.get(f"CSLB_{var}"):
-                    setattr(self, var.lower(), environ.get(f"CSLB_{var}"))
-                    logger.debug(
-                        f"ENV: Configured {var.lower()} = {'*****' if var == 'PROXMOX_PASS' else environ.get(f'CSLB_{var}')} (from CSLB_{var})"
-                    )
+    @percent_cpu.setter
+    def percent_cpu(self, percent_cpu: float = 0.4):
+        self._percent_cpu = percent_cpu
+        self.balance_resource_weights()
 
-            # Then override with CLI args
-            for k, v in args.items():
-                if v is not None:
-                    match k:
-                        case "exclude_node":
-                            self.exclude_nodes += v
-                        case "exclude_vmid":
-                            self.exclude_vmids += v
-                        case "exclude_type":
-                            self.exclude_types += v
-                        case "include_node":
-                            for x in self.exclude_nodes:
-                                if x in v:
-                                    self.exclude_nodes.remove(x)
-                        case "include_vmid":
-                            for x in self.exclude_vmids:
-                                if x in v:
-                                    self.exclude_vmids.remove(x)
-                        case "include_type":
-                            for x in self.exclude_types:
-                                if x in v:
-                                    self.exclude_types.remove(x)
-                        case "config_file":
-                            continue
-                        case _:
-                            setattr(self, k, v)
+    @property
+    def percent_mem(self) -> float:
+        return self._percent_mem
 
-                    logger.debug(
-                        f"CLI: Configured {k + 's' if re.match(pluralize_re_match, k) else k} = {'*****' if k == 'proxmox_pass' else v}"
-                    )
-        else:
-            raise ConfigurationError("Cannot continue, not fully configured.")
-
-        # Resolve proportions so they always sum to 1
-        if self.percent_cpu + self.percent_mem > 1:
-            if self.percent_cpu > self.percent_mem:
-                if self.percent_cpu > 1:
-                    self.percent_cpu = 1
-                self.percent_mem = 1 - self.percent_cpu
-            if self.percent_cpu < self.percent_mem:
-                if self.percent_mem > 1:
-                    self.percent_mem = 1
-                self.percent_cpu = 1 - self.percent_mem
-            if self.percent_cpu == self.percent_mem:
-                self.percent_cpu = 0.5
-                self.percent_mem = 0.5
-        if self.percent_cpu + self.percent_mem < 1:
-            if self.percent_cpu > self.percent_mem:
-                self.percent_cpu = 1 - self.percent_mem
-            if self.percent_mem > self.percent_cpu:
-                self.percent_mem = 1 - self.percent_cpu
-            if self.percent_cpu == self.percent_mem:
-                self.percent_cpu = 0.5
-                self.percent_mem = 0.5
+    @percent_mem.setter
+    def percent_mem(self, percent_mem: float = 0.6):
+        self._percent_mem = percent_mem
+        self.balance_resource_weights()
 
     def __dict__(self) -> dict:
         return {
-            "args": self.args,
             "config_file": self.config_file,
             "dry_run": self.dry_run,
             "verbose": self.verbose,
@@ -176,3 +91,26 @@ class Config:
             "proxmox_node": self.proxmox_node,
             "proxmox_port": self.proxmox_port,
         }
+
+    def balance_resource_weights(self):
+        # Resolve proportions so they always sum to 1
+        if self._percent_cpu + self._percent_mem > 1:
+            if self._percent_cpu > self._percent_mem:
+                if self._percent_cpu > 1:
+                    self._percent_cpu = 1
+                self._percent_mem = 1 - self._percent_cpu
+            if self._percent_cpu < self._percent_mem:
+                if self._percent_mem > 1:
+                    self._percent_mem = 1
+                self._percent_cpu = 1 - self._percent_mem
+            if self._percent_cpu == self._percent_mem:
+                self._percent_cpu = 0.5
+                self._percent_mem = 0.5
+        if self._percent_cpu + self._percent_mem < 1:
+            if self._percent_cpu > self._percent_mem:
+                self._percent_cpu = 1 - self._percent_mem
+            if self._percent_mem > self._percent_cpu:
+                self._percent_mem = 1 - self._percent_cpu
+            if self._percent_cpu == self._percent_mem:
+                self._percent_cpu = 0.5
+                self._percent_mem = 0.5
---
+++ SimpleCSLB/Runner.py
@@ -0,0 +1,324 @@
+#!/usr/Scripts/env python3
+
+#  Copyright (C) 2024 Travis Wichert
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from argparse import ArgumentParser
+from os import environ
+from re import compile
+from re import match as re_match
+from sys import exit, stdout
+
+from loguru import logger
+from yaml import safe_load
+
+from SimpleCSLB.Config import Config, ConfigurationError
+from SimpleCSLB.WorkloadBalancer import WorkloadBalancer
+
+__version__ = "1.3.0"
+__title__ = "pve-cslb"
+__copyright__ = """
+Copyright (C) 2024 Travis Wichert <padthaitofuhot@protonmail.com>
+"""
+__description__ = """
+A workload balancing engine for ProxmoxPVE.  Identifies nodes with imbalanced loads and migrates workloads around to even things out.
+"""
+
+re_pluralize = compile(r"^(include|exclude)_(node|type|vmid)$")
+re_include = compile(r"^include_.*$")
+re_exclude = compile(r"^exclude.*$")
+
+
+@logger.catch(level="ERROR")
+def main():
+
+    parser = ArgumentParser(
+        prog="pve-cslb",
+        description=f"{__title__} {__version__} - {__description__}",
+        epilog=__copyright__,
+    )
+    parser.add_argument(
+        "-c",
+        "--config-file",
+        metavar="FILE",
+        help=f"YAML configuration file (default: none)",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Increase verbosity (default: false)",
+    )
+    parser.add_argument(
+        "-q",
+        "--quiet",
+        action="store_true",
+        help="Only output errors (default: false)",
+    )
+    parser.add_argument(
+        "--no-color",
+        action="store_true",
+        help="Disable ANSI color in output (default: false)",
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="Perform read-only analysis; no write actions. (default: false)",
+    )
+    parser.add_argument(
+        "--proxmox-node",
+        metavar="NODE",
+        type=str,
+        help="Proxmox node (default: localhost)",
+    )
+    parser.add_argument(
+        "--proxmox-port",
+        metavar="PORT",
+        type=int,
+        help="Proxmox port (default: 8006)",
+    )
+    parser.add_argument(
+        "--proxmox-user",
+        metavar="USER",
+        type=str,
+        help="Proxmox user (default: root@pam)",
+    )
+    parser.add_argument(
+        "--proxmox-pass",
+        metavar="PASS",
+        type=str,
+        help="Proxmox password (no default)",
+    )
+    parser.add_argument(
+        "--max-migrations",
+        metavar="NUM",
+        type=int,
+        help="Max simultaneous migrations to start (default: 5)",
+    )
+    parser.add_argument(
+        "--percent-cpu",
+        metavar="%",
+        type=float,
+        help="Percent priority of CPU rule (p-cpu and p-mem must equal 1.0; default: 0.4)",
+    )
+    parser.add_argument(
+        "--percent-mem",
+        metavar="%",
+        type=float,
+        help="Percent priority of MEM rule (p-cpu and p-mem must equal 1.0; default: 0.6)",
+    )
+    parser.add_argument(
+        "--exclude-node",
+        action="append",
+        help="Exclude a node (can be specified multiple times)",
+    )
+    parser.add_argument(
+        "--exclude-vmid",
+        action="append",
+        help="Exclude a VMID (can be specified multiple times)",
+    )
+    parser.add_argument(
+        "--exclude-type",
+        action="append",
+        help="Exclude a workload type ('lxc' or 'qemu'; can be specified multiple times)",
+    )
+    parser.add_argument(
+        "--include-node",
+        action="append",
+        help="Include a previously excluded node (can be specified multiple times)",
+    )
+    parser.add_argument(
+        "--include-vmid",
+        action="append",
+        help="Include a previously excluded VMID (can be specified multiple times)",
+    )
+    parser.add_argument(
+        "--include-type",
+        action="append",
+        help="Include a previously excluded workload type (must be 'lxc' or 'qemu'; can be specified multiple times)",
+    )
+    args = vars(parser.parse_args())
+
+    log_level = "INFO"
+    if args["verbose"]:
+        log_level = "DEBUG"
+    if args["quiet"]:
+        log_level = "ERROR"
+
+    logging_config = {
+        "handlers": [
+            {
+                "sink": stdout,
+                "colorize": not args["no_color"],
+                "format": "<green>{time:YYYY-MM-DD HH:mm:ss}</green> <level>{message}</level>",
+                "level": log_level,
+                # "enqueue": True,
+            }
+        ],
+    }
+    logger.configure(**logging_config)
+    logger.enable("WorkloadBalancer")
+    logger.enable("Config")
+
+    if not args:
+        raise ConfigurationError("Cannot continue, not fully configured.")
+
+    lb_config = Config()
+    setattr(lb_config, "config_file", args["config_file"])
+
+    # First read from config file
+    if "config_file" in args.keys() and args["config_file"]:
+        logger.debug(f"CLI: Configured config_file = {args['config_file']}")
+        try:
+            with open(args["config_file"], "r") as stream:
+                config = safe_load(stream)
+                for k, v in config.items():
+                    setattr(lb_config, k, v)
+                    logger.debug(
+                        f"YML: Configured {k} = {'*****' if k == 'proxmox_pass' else v}"
+                    )
+        except FileNotFoundError:
+            raise ConfigurationError("Config file not found")
+
+    # Next configure from CLI and ENV
+    # CLI-only vars
+    for var in [
+        "verbose",
+        "quiet",
+        "no_color",
+    ]:
+        if var in args.keys():
+            if args[var] is not None:
+                setattr(lb_config, var, args[var])
+                logger.debug(f"CLI: Configured {var} = {args[var]}")
+
+    # Scalars
+    for var in [
+        "proxmox_node",
+        "proxmox_port",
+        "proxmox_user",
+        "proxmox_pass",
+        "percent_cpu",
+        "percent_mem",
+        "max_migrations",
+        "dry_run",
+    ]:
+        if environ.get(f"CSLB_{var.upper()}"):
+            setattr(lb_config, var, environ.get(f"CSLB_{var.upper()}"))
+            logger.debug(
+                f"ENV: Configured {var} = {'*****' if var == 'proxmox_pass' else environ.get(f'CSLB_{var.upper()}')} (from CSLB_{var.upper()})"
+            )
+
+        if var in args.keys():
+            if args[var] is not None:
+                setattr(lb_config, var, args[var])
+                logger.debug(
+                    f"CLI: Configured {var + 's' if re_match(re_pluralize, var) else var} = {'*****' if var == 'proxmox_pass' else args[var]}"
+                )
+
+    # Lists
+    for var in [
+        "exclude_node",
+        "exclude_vmid",
+        "exclude_type",
+        "include_node",
+        "include_vmid",
+        "include_type",
+    ]:
+        var_s = var + "s"
+        ex_var_s = var_s.replace("include", "exclude")
+
+        if environ.get(f"CSLB_{var_s.upper()}"):
+
+            if re_match(re_exclude, var):
+                env_list = environ.get(f"CSLB_{var_s.upper()}").split(" ")
+                cnf_list = getattr(lb_config, var_s)
+                new_list = cnf_list + env_list
+                setattr(lb_config, var_s, new_list)
+                logger.debug(
+                    f"ENV: Configured {var_s}: added {env_list} (from CSLB_{var_s.upper()})"
+                )
+                del env_list
+                del cnf_list
+                del new_list
+
+            if re_match(re_include, var):
+                env_list = environ.get(f"CSLB_{var_s.upper()}").split(" ")
+                cnf_list = getattr(lb_config, ex_var_s)
+                new_list = []
+                for item in cnf_list:
+                    if item in env_list:
+                        continue
+                    new_list.append(item)
+                setattr(lb_config, ex_var_s, new_list)
+                logger.debug(
+                    f"ENV: Configured {ex_var_s}: removed {env_list} (from CSLB_{var_s.upper()})"
+                )
+                del env_list
+                del cnf_list
+                del new_list
+
+        if var in args.keys() and args[var] is not None:
+
+            if re_match(re_exclude, var):
+                cnf_list = getattr(lb_config, var_s)
+                for item in args[var]:
+                    if item not in cnf_list:
+                        new_list = cnf_list + [item]
+                        setattr(lb_config, var_s, new_list)
+                        logger.debug(f"CLI: Configured {var_s}: added {item}")
+                del cnf_list
+
+            if re_match(re_include, var):
+                cnf_list = getattr(lb_config, ex_var_s)
+                for item in args[var]:
+                    if item in cnf_list:
+                        cnf_list.remove(item)
+                        setattr(lb_config, ex_var_s, cnf_list)
+                        logger.debug(f"CLI: Configured {ex_var_s}: removed {item}")
+                del cnf_list
+
+    my_simple_cslb = WorkloadBalancer(lb_config)
+    migration_candidates = my_simple_cslb.get_migration_candidates()
+
+    if len(migration_candidates) < 1:
+        logger.success("No migration candidate(s) found.")
+        exit(0)
+
+    if not lb_config.dry_run:
+        for migration_candidate in migration_candidates:
+            success, jobspec = my_simple_cslb.do_migration(migration_candidate)
+        logger.success("Migration job(s) submitted")
+        exit(0)
+    else:
+        logger.success("Dry run; no migration(s) started.")
+        exit(0)
+
+
+if __name__ == "__main__":
+    exit(main())
