733a2cd1db86b1bb269726cde1eff35a50f70fd4
padthaitofuhot <padthaitofuhot@users.noreply.github.com>
2025-02-22 16:56:01 -0500

    __dict__ is implied

--- src/pve_cslb/config.py
+++ src/pve_cslb/config.py
@@ -16,19 +16,6 @@ This module contains the Config class and helpers.
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from loguru import logger
 
@@ -46,41 +33,27 @@ class Config:
     """
 
     # pylint: disable=R0902
-    args: dict
-    config_file: str
-    dry_run: bool
-    verbose: bool
-    quiet: bool
-    no_color: bool
-    exclude_nodes: list
-    exclude_vmids: list
-    exclude_types: list
-    _tolerance: float
-    _percent_cpu: float
-    _percent_mem: float
-    max_migrations: int
-    proxmox_user: str
-    proxmox_pass: str
-    proxmox_node: str
-    proxmox_port: int
+    args: dict = {}
+    config_file: str = "/etc/pve-cslb.conf"
+    dry_run: bool = False
+    verbose: bool = False
+    quiet: bool = False
+    no_color: bool = False
+    exclude_nodes: list = []
+    exclude_vmids: list = []
+    exclude_types: list = []
+    _tolerance: float = 0.2
+    _percent_cpu: float = 0.4
+    _percent_mem: float = 0.6
+    max_migrations: int = 5
+    proxmox_user: str = "root@pam"
+    proxmox_pass: str = ""
+    proxmox_node: str = "localhost"
+    proxmox_port: int = 8006
+    proxmox_verify_ssl: bool = True
 
     def __init__(self):
-        self.config_file = ""
-        self.dry_run = False
-        self.verbose = False
-        self.quiet = False
-        self.no_color = False
-        self.exclude_nodes = []
-        self.exclude_vmids = []
-        self.exclude_types = []
-        self._tolerance = 0.2
-        self._percent_cpu = 0.4
-        self._percent_mem = 0.6
-        self.max_migrations = 5
-        self.proxmox_user = "root@pam"
-        self.proxmox_pass = ""
-        self.proxmox_node = "localhost"
-        self.proxmox_port = 8006
+        pass
 
     @property
     def tolerance(self) -> float:
@@ -113,24 +86,6 @@ class Config:
         """Property setter for _percent_mem"""
         self._percent_mem = percent_mem
         self.balance_resource_weights()
-
-    def __dict__(self) -> dict:
-        return {
-            "config_file": self.config_file,
-            "dry_run": self.dry_run,
-            "verbose": self.verbose,
-            "quiet": self.quiet,
-            "exclude_nodes": self.exclude_nodes,
-            "exclude_vmids": self.exclude_vmids,
-            "exclude_types": self.exclude_types,
-            "percent_cpu": self.percent_cpu,
-            "percent_mem": self.percent_mem,
-            "max_migrations": self.max_migrations,
-            "proxmox_user": self.proxmox_user,
-            "proxmox_pass": self.proxmox_pass,
-            "proxmox_node": self.proxmox_node,
-            "proxmox_port": self.proxmox_port,
-        }
 
     def balance_resource_weights(self):
         """Ensures resource weighting proportions always sum to 1"""
