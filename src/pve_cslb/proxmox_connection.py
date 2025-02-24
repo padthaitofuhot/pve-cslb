934073864bb7a11f0e1f1a63a39352c36f584995
padthaitofuhot <padthaitofuhot@users.noreply.github.com>
2025-02-23 20:19:47 -0500

    merge API connection message with notification

--- src/pve_cslb/proxmox_connection.py
+++ src/pve_cslb/proxmox_connection.py
@@ -70,12 +70,11 @@ class ProxmoxConnection(ProxmoxAPI):
                     "backend": "local",
                 }
 
-        logger.info(
-            f"Using Proxmox API via {self.config.proxmox_scheme}://{self.config.proxmox_node}"
-        )
         try:
             super().__init__(**kwargs)
-            logger.debug("Proxmox API connection established.")
+            logger.info(
+                f"Proxmox API connected via {self.config.proxmox_scheme}://{self.config.proxmox_node}"
+            )
         except (ResourceException, ConnectionError, AuthenticationError) as exception:
             logger.error(exception)
             raise
